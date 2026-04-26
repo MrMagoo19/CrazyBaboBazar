f00cd0d708736c6926862bf9508526db62aa3e8c
MrMagoo19 <roland.mueller.biz@gmail.com>
2026-04-26 16:08:21 +0200

    feat: use Logo_4 in hero

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -57,7 +57,7 @@ export default async function HomePage() {
           {/* Löwe mittig */}
           <div className="shrink-0 z-10">
             <Image
-              src="/Logo_3.png"
+              src="/Logo_4.png"
               alt="Crazy Babo Bazar"
               width={500}
               height={334}
